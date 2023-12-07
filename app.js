1f3359671a47fad432c99d8e382515653bbaa9d5
thisisal1ev <aaalievvv1@gmail.com>
2023-12-07 14:53:09 +0500

    main

---
+++ app.js
@@ -0,0 +1,19 @@
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voicesSelect = document.querySelector('select');
+
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  speech.voice = voices[0];
+
+  voices.forEach((voice, i) => (voicesSelect.options[i]) = new Option(voice.name, i))
+};
+
+document.querySelector('button').addEventListener('click', () => {
+  speech.text = document.querySelector('textarea').value;
+  window.speechSynthesis.speak(speech);
+});
+
+voicesSelect.addEventListener('change', () => {
+  speech.voice = voices[voicesSelect.value];
+});
